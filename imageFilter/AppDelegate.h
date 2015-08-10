5bdbcd72576062bde9344981185b5522a4be2dee
qingjin <qingjin@wacai.com>
2015-08-10 09:46:26 +0800

    Initial Commit

---
+++ imageFilter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  imageFilter
+//
+//  Created by qianhongqiang on 15/8/10.
+//  Copyright (c) 2015年 qianhongqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ imageFilter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  imageFilter
+//
+//  Created by qianhongqiang on 15/8/10.
+//  Copyright (c) 2015年 qianhongqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
